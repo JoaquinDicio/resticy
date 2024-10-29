2ffd206fa5b87b9efaf85cb27eff03f83520c7ff
Nico :D <nicopereyra855@gmail.com>
2024-10-29 17:40:50 -0300

    El login maneja errores  y loguea [Falta agregar ruta hacia donde redirige]

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import InputField from "./InputField";
 import useAuth from "../hooks/useAuth";
 
 const LoginForm = () => {
-  const formData = {};
-  const { authenticate, error, isLoading } = useAuth();
+  const [formData, setFormData] = useState({})
+  const { authenticate, errors, isLoading } = useAuth();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
   async function handleSubmit(e) {
     e.preventDefault();
     await authenticate(formData);
   }
+
+  useEffect(() => {
+    if (errors.credentialsError) {
+      console.log("Errores de autenticación:", errors.credentialsError);
+    }
+  }, [errors]);
+  
 
   return (
     <form
@@ -18,14 +33,19 @@ const LoginForm = () => {
       method="POST"
       className="flex flex-col"
     >
+      {errors.credentialsError && <p className="text-red-500">{errors.credentialsError}</p>}
       <InputField
         label="Email"
         type="email"
+        name="email"
+        onChange={handleChange}
         placeholder="TuEmpresa@gmail.com"
       />
       <InputField
         label="Contraseña"
         type="password"
+        name="password"
+        onChange={handleChange}
         placeholder="Ingresa una contraseña"
       />
       <div className="flex flex-col md:flex-row items-center gap-5 mt-5">
--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -4,14 +4,30 @@ import { useNavigate } from "react-router-dom";
 
 export default function useAuth() {
   const [isLoading, setLoading] = useState(false);
-  const [errors, setErrors] = useState({ emailError: null, passwordError: null, nameError: null });  
+  const [errors, setErrors] = useState({ emailError: null, passwordError: null, nameError: null, credentialsError: null });  
   const navigate = useNavigate();
 
   const authenticate = async (formData) => {
     // TODO : peticion al backend
     // esta funcion puede terminar en una redireccion o bien en un cambio de estado
     // los errores deben setearse en un state al igual que el loader
+
     setLoading(true);
+
+    try{
+
+      await axios.post("http://localhost:8080/login", formData);
+      console.log('Se redirije al dashboard [Todavia no se creo]')
+
+      navigate("#");
+    }catch(error){
+      const errorData = error.response.data; 
+      setErrors(prev => ({
+        credentialsError: errorData || prev.credentialsError,
+      }));
+    }
+
+    setLoading(false)
   };
 
   const register = async (formData) => {
@@ -23,7 +39,6 @@ export default function useAuth() {
       navigate("/login");
     } catch (error) {
       const errorData = error.response.data; 
-      
       console.log(errorData)
 
       if (errorData) {
