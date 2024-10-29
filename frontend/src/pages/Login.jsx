4e21e803777d1db352f60a67de3a8963a553dd2e
joacodicio <joacodicio@gmail.com>
2024-10-29 14:43:48 -0300

    arreglos tailwind

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,13 +4,20 @@ import LoginForm from "../components/LoginForm";
 const Login = () => {
   return (
     <main>
-      <div className="w-[100%] h-screen bg-[#51161F] flex justify-center items-center">
-        <div className="flex flex-col lg:flex-row w-[70vw] h-[70vh] rounded-lg">
-          <img className="h-[100%] overflow rounded-l-lg hidden md:block" src={'../assets/imgForm.jpg'} alt="" />
+      <div className="w-[100%] min-h-screen bg-[#51161F] flex justify-center items-center">
+        <div className="flex flex-col lg:flex-row w-[70vw] rounded-lg">
+          <img
+            className="max-w-[400px] w-full overflow rounded-l-lg hidden md:block"
+            src={"../assets/imgForm.jpg"}
+            alt=""
+          />
           <div className="p-10 md:p-20 bg-white w-full md:w-[60%] rounded-r-lg">
             <div className="mb-9">
               <p className="text-2xl">
-              Iniciar Sesion o <a href="#" className="text-blue-800">Registrarse</a>
+                Iniciar Sesion o{" "}
+                <a href="#" className="text-blue-800">
+                  Registrarse
+                </a>
               </p>
             </div>
             <LoginForm />
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,13 +4,20 @@ import RegisterForm from "../components/RegisterForm";
 const Register = () => {
   return (
     <main>
-      <div className="w-[100%] h-screen bg-[#51161F] flex justify-center items-center">
-        <div className="flex flex-col lg:flex-row w-[70vw] h-[70vh] rounded-lg">
-          <img className="h-[100%] overflow rounded-l-lg hidden md:block" src={'../assets/imgForm.jpg'} alt="" />
+      <div className="w-full min-h-screen bg-[#51161F] flex justify-center items-center">
+        <div className="flex flex-col lg:flex-row w-[70vw] rounded-lg">
+          <img
+            className=" max-w-[400px] overflow rounded-l-lg hidden md:block"
+            src={"../assets/imgForm.jpg"}
+            alt=""
+          />
           <div className="p-10 md:p-20 bg-white w-full md:w-[60%] rounded-r-lg">
             <div className="mb-9">
               <p className="text-2xl">
-                Registrarse o <a href="#" className="text-blue-800">Iniciar Sesion</a>
+                Registrarse o{" "}
+                <a href="#" className="text-blue-800">
+                  Iniciar Sesion
+                </a>
               </p>
             </div>
             <RegisterForm />
