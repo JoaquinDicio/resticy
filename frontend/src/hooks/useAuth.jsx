1a5faecec3b22f1d61652c6e942f1cf9ecfc24ee
Nico :D <nicopereyra855@gmail.com>
2024-10-29 17:45:21 -0300

    Borrado de comments

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -8,17 +8,11 @@ export default function useAuth() {
   const navigate = useNavigate();
 
   const authenticate = async (formData) => {
-    // TODO : peticion al backend
-    // esta funcion puede terminar en una redireccion o bien en un cambio de estado
-    // los errores deben setearse en un state al igual que el loader
 
     setLoading(true);
-
     try{
-
       await axios.post("http://localhost:8080/login", formData);
       console.log('Se redirije al dashboard [Todavia no se creo]')
-
       navigate("#");
     }catch(error){
       const errorData = error.response.data; 
@@ -26,29 +20,27 @@ export default function useAuth() {
         credentialsError: errorData || prev.credentialsError,
       }));
     }
-
     setLoading(false)
+
   };
 
   const register = async (formData) => {
 
     setLoading(true);
-
     try {
       await axios.post("http://localhost:8080/register", formData);
       navigate("/login");
     } catch (error) {
       const errorData = error.response.data; 
-      console.log(errorData)
 
       if (errorData) {
         setErrors(prev => ({
           emailError: errorData || prev.emailError,
         }));
       }
-      
     }
     setLoading(false);
+
   };
 
   return { authenticate, register, errors, isLoading };
