4c2b915106f9e0318bb5e1906b75cadcc2479244
Nico :D <nicopereyra855@gmail.com>
2025-01-18 14:15:57 -0300

    Section PlansLanding Complete

--- frontend/src/components/LandingPage/PlansLanding.jsx
+++ frontend/src/components/LandingPage/PlansLanding.jsx
@@ -53,7 +53,7 @@ const PricingPlans = () => {
   ];
 
   return (
-    <div className="bg-yellow-400 px-10 pt-10 md:p-10">
+    <div className="bg-yellow-400 px-10 pt-10 md:pb-10">
       <div className="md:hidden">
       <Swiper
           slidesPerView={1}
@@ -63,11 +63,36 @@ const PricingPlans = () => {
         >
           {plans.map((plan, index) => (
             <SwiperSlide key={index}>
-              <div className="bg-white min-h-[420px] mb-10 rounded-lg shadow-lg p-6 flex flex-col justify-between">
-                <a href="#" className="text-blue-500 mt-6 font-medium underline">
-                  {plan.linkText}
-                </a>
-              </div>
+              <div
+              key={index}
+              className="bg-white rounded-lg shadow-lg px-5 flex flex-col p-5 justify-between min-h-[500px]  md:w-1/3"
+            >
+              <div>
+                  <div className="flex gap-3 items-center">
+                    <a href="#">
+                      <input type="radio" name="group-1"/>
+                    </a>
+                    <h2 className="text-xl font-bold">{plan.title}</h2>
+                  </div>
+                  <p className="text-4xl font-bold text-black mt-4">
+                    {plan.price} <span className="text-lg">{plan.period}</span>
+                  </p>
+                  <p className="text-gray-700 mt-4 text-md">{plan.description}</p>
+                  <ul className="list-inside text-gray-600 mt-4 text-sm">
+                    {plan.features.map((feature, index) => (
+                      <li key={index} className="mt-2 text-indent">
+                          <div className="flex">
+                          <span className="pr-2">•</span>
+                          {feature}
+                          </div>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              <a href="#" className="text-blue-500 mt-6 font-medium underline">
+                {plan.linkText}
+              </a>
+            </div>
             </SwiperSlide>
           ))}
         </Swiper>
@@ -75,18 +100,23 @@ const PricingPlans = () => {
       
       {/* Diseño en tarjetas para pantallas grandes */}
 
-      <div className="hidden md:flex flex-row justify-center gap-6 justify-center max-w-[60vw] mx-auto">
+      <div className="hidden md:flex flex-row justify-center gap-6 justify-center md:max-w-[80vw] lg:md:max-w-[60vw] mx-auto">
         {plans.map((plan, index) => (
             <div
               key={index}
               className="bg-white rounded-lg shadow-lg p-6 flex flex-col justify-between  md:w-1/3"
             >
               <div>
-                  <h2 className="text-xl font-bold">{plan.title}</h2>
+                  <div className="flex gap-3 items-center">
+                    <a href="#">
+                      <input type="radio" name="group-1"/>
+                    </a>
+                    <h2 className="text-xl font-bold">{plan.title}</h2>
+                  </div>
                   <p className="text-4xl font-bold text-black mt-4">
                     {plan.price} <span className="text-lg">{plan.period}</span>
                   </p>
-                  <p className="text-gray-700 mt-4 ">{plan.description}</p>
+                  <p className="text-gray-700 mt-4 text-md">{plan.description}</p>
                   <ul className="list-inside text-gray-600 mt-4 text-sm">
                     {plan.features.map((feature, index) => (
                       <li key={index} className="mt-2 text-indent">
