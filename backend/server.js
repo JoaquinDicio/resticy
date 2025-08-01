a31608af069c554002fc73b2bb4b3a33c4fc5bf6
joaquindicio <joacodicio@gmail.com>
2025-08-01 13:08:51 -0300

    [fix] changed cors policy

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ sequelize.sync({ alter: true });
 
 const server = http.createServer(app); // crea un servidor http
 const io = new SocketServer(server, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "https://resticy-frontend-jzxl9knto-joaquindicios-projects.vercel.app/" },
 });
 
 app.use(cors());
