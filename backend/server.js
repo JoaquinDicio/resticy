8df7ed93b70e5c7f500ea58d2b0dd52e0924102f
joaquindicio <joacodicio@gmail.com>
2025-10-28 19:44:45 -0300

    added localhost to cors origin

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,43 @@
-import express from 'express';
-import http from "http"
-import cors from "cors"
-import { mountRouters } from './routes/index.js';
-import { Server as Socket } from 'socket.io'
-import initModels from './models/index.js'
-import sequalize from './database.js';
-import seedRoles from './seeders/seedRoles.js';
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { mountRouters } from "./routes/index.js";
+import { Server as Socket } from "socket.io";
+import initModels from "./models/index.js";
+import sequalize from "./database.js";
+import seedRoles from "./seeders/seedRoles.js";
 
-const app = express()
+const app = express();
 
 //middlewares
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"] }));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/uploads', express.static('public/uploads'))
+app.use("/uploads", express.static("public/uploads"));
 
 mountRouters(app);
 
 //configuracion inicial de la base datos
 async function initDatabase() {
-    initModels()
-    await sequalize.sync({ alter: true })
-    await seedRoles()
+  initModels();
+  await sequalize.sync({ alter: true });
+  await seedRoles();
 }
 
-initDatabase()
-
+initDatabase();
 
 //inicializa el servidor y el socket
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 server.listen(process.env.PORT, () => {
-    console.log("Server running at", process.env.PORT)
-})
-
-const io = new Socket(server, {
-    cors: {
-        origin: "http://localhost:5173",
-    },
+  console.log("Server running at", process.env.PORT);
 });
 
-export { io }
+const io = new Socket(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+export { io };
