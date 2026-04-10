e14cd887c63be06fb5624f5a565df06ea81fd41a
JoaquinDicio <97567954+JoaquinDicio@users.noreply.github.com>
2026-04-10 12:53:33 -0300

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined");
 }
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequalize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     logging: false,
     dialectOptions: {
@@ -18,4 +18,4 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     }
 });
 
-export default sequelize;
+export default sequalize;
